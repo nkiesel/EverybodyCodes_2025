c33a35c7c5d4fadf15935d9c4e154adafbabffed
Norbert Kiesel <nk@iname.com>
2026-03-16 21:48:59 -0700

    quest10

--- src/test/kotlin/Quest10.kt
+++ src/test/kotlin/Quest10.kt
@@ -1,26 +1,14 @@
 import de.infix.testBalloon.framework.core.testSuite
 import io.kotest.matchers.shouldBe
+import java.nio.file.Files.move
 
 object Quest10 {
     private fun parse(input: List<String>) = CharArea(input)
 
-    private fun move(start: Point, area: CharArea, prev: Set<Point>): Set<Point> {
-        return buildSet {
-            for ((x, y) in listOf(-2 to -1, -2 to 1, 2 to -1, 2 to 1, -1 to -2, -1 to 2, 1 to -2, 1 to 2)) {
-                val n = start.move(x, y)
-                if (area.valid(n) && n !in prev) add(n)
-            }
-        }
-    }
-
-    private fun move2(start: Point, area: CharArea): Set<Point> {
-        return buildSet {
-            for ((x, y) in listOf(-2 to -1, -2 to 1, 2 to -1, 2 to 1, -1 to -2, -1 to 2, 1 to -2, 1 to 2)) {
-                val n = start.move(x, y)
-                if (area.valid(n)) add(n)
-            }
-        }
-    }
+    private fun move(start: Point, area: CharArea, prev: Set<Point> = emptySet()): Set<Point> =
+        listOf(-2 to -1, -2 to 1, 2 to -1, 2 to 1, -1 to -2, -1 to 2, 1 to -2, 1 to 2)
+            .map { (x, y) -> start.move(x, y) }
+            .filter { it !in prev && area.valid(it) }.toSet()
 
     fun one(input: List<String>, rounds: Int = 4): Int {
         val area = parse(input)
@@ -28,11 +16,7 @@ object Quest10 {
         var pos = setOf(dragon)
         val all = mutableSetOf(dragon)
         repeat(rounds) {
-            pos = buildSet {
-                for (p in pos) {
-                    addAll(move(p, area, all))
-                }
-            }
+            pos = pos.flatMap { move(it, area, all) }.toSet()
             all += pos
         }
         val count = all.count { area[it] == 'S' }
@@ -48,11 +32,7 @@ object Quest10 {
         var pos = setOf(dragon)
         var result = 0
         repeat(rounds) {
-            pos = buildSet {
-                for (p in pos) {
-                    addAll(move(p, area, emptySet()))
-                }
-            }
+            pos = pos.flatMap { move(it, area) }.toSet()
             val sheep = area.tiles('S').toMutableSet()
             val eaten = mutableSetOf<Point>()
             sheep.forEach { sheep ->
@@ -74,8 +54,8 @@ object Quest10 {
         return result
     }
 
-    fun three(input: List<String>): String {
-        return ""
+    fun three(input: List<String>): Long {
+        return 0L
     }
 }
 
@@ -128,9 +108,50 @@ val Quest10Test by testSuite {
         }
 
         test("three") {
-            val sample = """
+            val sample1 = """
+                SSS
+                ..#
+                #.#
+                #D.
             """.trimIndent().lines()
-            three(sample) shouldBe ""
+            three(sample1) shouldBe 15L
+
+            val sample2 = """
+                SSS
+                ..#
+                ..#
+                .##
+                .D#
+            """.trimIndent().lines()
+            three(sample2) shouldBe 8L
+
+            val sample3 = """
+                ..S..
+                .....
+                ..#..
+                .....
+                ..D..
+            """.trimIndent().lines()
+            three(sample3) shouldBe 44L
+
+            val sample4 = """
+                .SS.S
+                #...#
+                ...#.
+                ##..#
+                .####
+                ##D.#
+            """.trimIndent().lines()
+            three(sample4) shouldBe 4406L
+
+            val sample5 = """
+                SSS.S
+                .....
+                #.#.#
+                .#.#.
+                #.D.#
+            """.trimIndent().lines()
+            three(sample5) shouldBe 13033988838L
 
 //            val input = lines(quest, 3)
 //            three(input) shouldBe ""
